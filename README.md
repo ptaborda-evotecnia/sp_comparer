d61318c1df707f4d54d71a9b6ee18d3af3a89ed0
pablogtab <110042515+pablogtab@users.noreply.github.com>
2023-10-12 11:24:31 -0300

    open meld

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-{
-    "ignoreFoldersAtLoad":  ["servired", "regional"]
-}
+# MSSQL Server Stored Procedure Analyzer
+
+[![npm version](https://img.shields.io/npm/v/sp-comparer.svg)](https://www.npmjs.com/package/sp-comparer)
+[![License](https://img.shields.io/npm/l/sp-comparer.svg)](https://github.com/yourusername/sp-comparer/blob/main/LICENSE)
+
+A tool for analyzing stored procedures in MSSQL Server databases.
+
+## Installation
+
+You can install this package via npm:
+
+```bash
+npm install sp-comparer --save-dev
+
+
--- bin/index.js
+++ bin/index.js
@@ -1,21 +1,47 @@
 #!/usr/bin/env node
 const main = require("../index");
 const minimist = require('minimist');
-const { downloadAllStoredProcedures } = require("../lib/getProcedure");
 const { keyPress } = require("../lib/keypress");
-const { compararScriptsConCodigo } = require("../lib/inquirerFunctions");
+const { compararScriptsConCodigo, seleccionarPrompt } = require("../lib/inquirerFunctions");
+const { descargarScriptsFaltantes } = require("../lib/descargarScriptsFaltantes");
+const { compararAllProcedimientos } = require("../lib/compareProcedure");
+const { buscarScript, getProcedureDefinition } = require("../lib/getProcedure");
+const { openMeldFileVsContent } = require("../lib/openMeld");
 
 const argv = minimist(process.argv.slice(2));
 
 
 async function tmpMain() {
     if (argv.init) {
-        await downloadAllStoredProcedures('./scripts/')
+        await descargarScriptsFaltantes('./scripts/')
         await keyPress()
         console.clear()
         console.log('Comparar scripts con el código')
         await compararScriptsConCodigo()
         await keyPress()
+    }
+    if (argv.test) {
+        let resultado = await compararAllProcedimientos()
+
+        const scriptComparados = await Promise.all(
+            Object.keys(resultado)
+                .filter((conexion) => resultado[conexion]?.diferentes?.length)
+                .flatMap((conexion) =>
+                    resultado[conexion].diferentes.map(async (sp) => {
+                        return { ...(await buscarScript('scripts', conexion, sp, 'regional')), spOriginal: sp, conexion };
+                    })
+                )
+        );
+        console.clear();
+        console.log('Los siguientes sps son diferentes, seleccione los que desea impactar desde el codigo a la bbdd. \n')
+        let scriptAImpactar = await seleccionarPrompt(scriptComparados, 'Seleccionar', s => s.path)
+
+        console.log(scriptAImpactar)
+        let procedureBDDefinition = await getProcedureDefinition(scriptAImpactar.conexion, scriptAImpactar.spOriginal)
+
+        console.log(await openMeldFileVsContent(scriptAImpactar.fullPath, procedureBDDefinition))
+
+        process.exit()
     }
     main()
 }
